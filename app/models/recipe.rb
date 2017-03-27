34e6a76e832d22d8294efd60a900f48002ea91b7
Jack Smollen <jsmollen2@gmail.com>
2017-03-27 19:29:19 -0400

    Add user and finish styles

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
+	belongs_to :user
 
-
-	has_many :ingredients
-	has_many :directions
+	has_many :ingredients, inverse_of: :recipe
+	has_many :directions, inverse_of: :recipe
 
 	accepts_nested_attributes_for :ingredients,
   															reject_if: proc { |attributes| attributes['name'].blank? },
